765311200ea0ffb4aeebfa34eb4b34d4b7824fad
Ren-Mor <renatomorelli@hotmail.it>
2025-05-16 13:02:55 +0200

    added fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,13 +27,20 @@ function Navbar() {
               )
             )}
           </ul>
-          <div className="d-flex align-items-center">
-            <i className="bi bi-search icons"></i>
-            <div id="kids" className="fw-bold">
-              KIDS
+          <div className="d-flex flex-column align-items-center">
+            <div className="d-flex align-items-center mb-2">
+              <i className="bi bi-search icons"></i>
+              <div id="kids" className="fw-bold ms-3 me-3">
+                KIDS
+              </div>
+              <i className="bi bi-bell icons ms-2"></i>
+              <i className="bi bi-person-circle icons ms-2"></i>
             </div>
-            <i className="bi bi-bell icons"></i>
-            <i className="bi bi-person-circle icons"></i>
+
+            <div className="d-flex flex-row align-items-center mt-3">
+              <i className="bi bi-grid icons me-2"></i>
+              <i className="bi bi-grid-3x3 icons"></i>
+            </div>
           </div>
         </div>
       </div>
